0290d22f3d8227304cec9f4097100c74c5703ffa
Gerardo Salazar <gsalaz9800@gmail.com>
2023-11-16 00:08:40 -0600

    More performant buttons/toggles, graph highlighting working
    Add MIT license

--- PromPlot.jl
+++ PromPlot.jl
@@ -2,4 +2,4 @@ include("prometheus.jl")
 include("frontend.jl")
 
 p = PrometheusQueryClient()
-init_window(p; series_limit=20)
+init_window(p; series_limit=25)
--- frontend.jl
+++ frontend.jl
@@ -6,11 +6,17 @@ include("utils.jl")
 
 
 const FONT_COURIER = Makie.to_font("Courier")
-const MAX_LEGEND_CHAR_LIMIT = 180
+
+const MAX_LEGEND_ENTRY_CHAR_LIMIT = 40
+const MAX_LEGEND_CHAR_LIMIT = 140
 
 const RED::RGBAf = RGBAf(1.0, 0.0, 0.0, 1.0)
 const WHITE::RGBAf = RGBAf(1.0, 1.0, 1.0, 1.0)
 const VERY_DARK::RGBAf = RGBAf(0.1, 0.1, 0.1, 1.0)
+
+const NORMAL_CURSOR = GLMakie.GLFW.CreateStandardCursor(GLMakie.GLFW.ARROW_CURSOR)
+const HAND_CURSOR = GLMakie.GLFW.CreateStandardCursor(GLMakie.GLFW.HAND_CURSOR)
+
 
 function format_label_table(labels::Vector{String})::Vector{String}
     # Takes a vector of strings split by ',' and returns a vector of strings
@@ -28,16 +34,28 @@ function format_label_table(labels::Vector{String})::Vector{String}
     formatted_labels = []
     for label in labels
         final_label = ""
+        label_count = length(split(label, ","))
+
         for (idx, col) in enumerate(split(label, ","))
-            if idx < length(split(label, ","))
-                final_label *= lpad(col, cols[idx]) * ", "
+            # Truncate per column so that we can fit more columns in the legend
+            if length(col) > MAX_LEGEND_ENTRY_CHAR_LIMIT
+                col = col[begin:MAX_LEGEND_ENTRY_CHAR_LIMIT - 3] * "..."
+            end
+
+            if idx < label_count
+                # Get the minimum of the column width or the max entry, since we are
+                # going to be truncating the label anyway
+                final_label *= lpad(col, min(cols[idx], MAX_LEGEND_ENTRY_CHAR_LIMIT)) * ", "
             else
                 final_label *= col
             end
         end
 
         if length(final_label) > MAX_LEGEND_CHAR_LIMIT
-            final_label = final_label[begin:MAX_LEGEND_CHAR_LIMIT - 3] * "..."
+            final_label = final_label[begin:MAX_LEGEND_CHAR_LIMIT - 3]
+            if !endswith(final_label, "...")
+                final_label *= "..."
+            end
         end
         push!(formatted_labels, final_label)
     end
@@ -45,12 +63,8 @@ function format_label_table(labels::Vector{String})::Vector{String}
     return formatted_labels
 end
 
-function render_data(fig, layout, ax, df, status_label; is3d=false, series_limit=100)
+function render_data(fig, layout, ax, df, status_label; page=1, is3d=false, series_limit=100)
     try empty!(ax); catch _; end;
-    # TODO: the legend is recreated each time we update the plot, and not 
-    # thrown away to be collected, so we have duplicate data and it gets glitchy
-    try delete!(layout[1, 2]); catch _; end;
-    try delete!(layout[2, 2]); catch _; end;
 
     dfgroups = groupby(df, allbut(df, [:ts, :value]))
     series = []
@@ -94,51 +108,110 @@ function render_data(fig, layout, ax, df, status_label; is3d=false, series_limit
     end
 
     toggles = [
-        Toggle(
-            fig;
-            active=false,
-            framecolor_active=:black,
-            framecolor_inactive=VERY_DARK,
+        Button(
+            layout[1, 2];
+            label="",
             buttoncolor=series[idx].color
         ) for idx in labels |> eachindex
     ]
     
     formatted_labels = [
         Label(
-            fig;
+            layout[1, 2];
             text=i,
             font=FONT_COURIER,
             fontsize=14
         ) for i in format_label_table(labels)
     ]
 
+    series_indexes = Dict()
+    for (idx, s) in enumerate(series)
+        series_indexes[s] = idx
+    end
+
+
+    # Pagination for the Legend
+    prev_button = Button(layout[2, 2]; label="Previous", buttoncolor=:blue)
+    next_button = Button(layout[2, 2]; label="Next", buttoncolor=:blue)
+
     layout[1, 2] = grid!(hcat(toggles, formatted_labels))
-    # TODO: add pagination to the Legend
-    #layout[2, 2] = Button(fig, label="Next >")
+    layout[2, 2] = grid!(hcat(prev_button, next_button))
 
     colsize!(layout, 1, Aspect(1, 1))
 
-    on(events(fig).mousebutton) do e
-        if e.action !== Mouse.release
-            return Consume(false)
-        end
+    glscreen = display(fig).glscreen
 
-        # Scan all of the active toggles and hide series that are inactive.
-        # We show all series if all toggles are set to inactive.
-        if !all(x -> x.active.val, toggles)
+    for b in toggles
+        on(b.clicks) do e
+            # Scan all of the active toggles and hide series that are inactive.
+            # We show all series if all toggles are set to inactive.
+            if all(x -> x.clicks.val % 2 == 0, toggles)
+                for (idx, t) in enumerate(toggles)
+                    series_visible[idx][] = true
+                    t.buttoncolor[] = RGBAf(t.buttoncolor.val.r, t.buttoncolor.val.g, t.buttoncolor.val.b, 1.0)
+                end
+                return Consume(false)
+            end
+
             for (idx, t) in enumerate(toggles)
-                series_visible[idx][] = true
+                # The number of clicks will be odd if the toggle is active
+                is_active = t.clicks[] % 2 == 1
+                series_visible[idx][] = is_active
+                t.buttoncolor[] = RGBAf(
+                    t.buttoncolor.val.r,
+                    t.buttoncolor.val.g,
+                    t.buttoncolor.val.b,
+                    is_active ? 1.0 : 0.25)
             end
 
             return Consume(false)
         end
 
-        for (idx, t) in enumerate(toggles)
-            series_visible[idx][] = t.active.val
-        end
+        active = nothing
+        requires_reset = false
+        on(events(fig).mouseposition) do m
+            p, _ = pick(fig)
 
-        return Consume(false)
+            if active != p && !isnothing(active)
+                active.linewidth[] = 1.5
+                active = nothing
+            end
+
+            if isnothing(p)
+                GLMakie.GLFW.SetCursor(glscreen, NORMAL_CURSOR)
+                if requires_reset
+                    for t in toggles 
+                        t.buttoncolor[] = RGBAf(t.buttoncolor.val.r, t.buttoncolor.val.g, t.buttoncolor.val.b, 1.0)
+                    end
+                end
+
+                requires_reset = false
+                return Consume(false)
+            end
+
+            if p in series
+                p.linewidth[] = 5.0
+                active = p
+                GLMakie.GLFW.SetCursor(glscreen, HAND_CURSOR)
+                requires_reset = true
+                # Darken the rest of the legends to show which value we have selected
+                seriesidx = series_indexes[p]
+
+                for (idx, t) in enumerate(toggles)
+                    t.buttoncolor[] = RGBAf(
+                        t.buttoncolor.val.r,
+                        t.buttoncolor.val.g,
+                        t.buttoncolor.val.b,
+                        idx == seriesidx ? 1.0 : 0.25
+                    )
+                end
+            end
+
+            return Consume(false)
+        end
     end
+
+    return vcat(toggles, formatted_labels, prev_button, next_button)
 end
 
 function validate_df(df, status_label; update_text::Bool=true)::Bool
@@ -177,27 +250,24 @@ function init_window(p::PrometheusQueryClient; is3d=false, series_limit=100)
 
     colsize!(plot_layout, 1, Aspect(1, 1))
 
-    prev_toggle_status = false
-    is3d_toggle = Toggle(
+    is3d_button = Button(
         fig;
-        active=prev_toggle_status,
-        framecolor_active=:black,
-        framecolor_inactive=VERY_DARK
+        label="3D Plot",
+        buttoncolor=:blue,
+        labelcolor=:white,
     )
 
-    live_mode_toggle = Toggle(
+    live_mode_button = Button(
         fig;
-        active=false,
-        framecolor_active=:black,
-        framecolor_inactive=VERY_DARK
+        label="Live Data",
+        buttoncolor=:blue,
+        labelcolor=:white,
     )
 
     user_layout[1, 1] = grid!(
         hcat(
-            is3d_toggle,
-            Label(fig, "3D Plot", fontsize=16),
-            live_mode_toggle,
-            Label(fig, "Live Data", fontsize=16)
+            is3d_button,
+            live_mode_button,
         )
     )
 
@@ -220,21 +290,31 @@ function init_window(p::PrometheusQueryClient; is3d=false, series_limit=100)
     )
 
     df::Union{Nothing, DataFrame} = nothing
+    gc = nothing
+    is3d = false
 
-    on(events(fig).mousebutton) do _ 
-        if prev_toggle_status == is3d_toggle.active[]
-            return Consume(false)
-        end
-
+    on(is3d_button.clicks) do clicks
         delete!(ax)
 
-        prev_toggle_status = is3d_toggle.active[]
-        ax = prev_toggle_status ? 
+        is3d = clicks % 2 != 0
+        ax = is3d ?
             Axis3(plot_layout[1, 1]) :
             Axis(plot_layout[1, 1])
 
+        # Counter-intuitive, but if we toggled 3D, we want to have the option to revert
+        if is3d
+            is3d_button.label[] = "2D Plot"
+        else
+            is3d_button.label[] = "3D Plot"
+        end
+
         if validate_df(df, status_label; update_text=false)
-            render_data(fig, plot_layout, ax, df, status_label, is3d=is3d_toggle.active[], series_limit=series_limit)
+            if !isnothing(gc)
+                for g in gc
+                    delete!(g)
+                end
+            end
+            gc = render_data(fig, plot_layout, ax, df, status_label, is3d=is3d, series_limit=series_limit)
         end
 
         return Consume(false)
@@ -254,7 +334,12 @@ function init_window(p::PrometheusQueryClient; is3d=false, series_limit=100)
         end
 
         if validate_df(df, status_label; update_text=true)
-            render_data(fig, plot_layout, ax, df, status_label, is3d=is3d_toggle.active[], series_limit=series_limit)
+            if !isnothing(gc)
+                for g in gc
+                    delete!(g)
+                end
+            end
+            gc = render_data(fig, plot_layout, ax, df, status_label, is3d=is3d, series_limit=series_limit)
         end
 
         return Consume(false)
