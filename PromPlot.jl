02777562a27f7c8c92208da31258bea9c1337128
Gerardo Salazar <gsalaz9800@gmail.com>
2023-11-16 17:32:49 -0600

    Add additional interactivity features for basic plot selection/filtering
      * Add new example images for README.md
      * Add interactivity to Legend Labels
      * Add interactivity directly to Plots
      * Add Start Date field
      * Add End Date field
      * Add ability to select Step size for Prometheus
      * Various bug fixes and performance improvements

--- PromPlot.jl
+++ PromPlot.jl
@@ -1,5 +1,5 @@
 include("prometheus.jl")
-include("frontend.jl")
+include("frontend_gui.jl")
 
-p = PrometheusQueryClient()
-init_window(p; series_limit=25)
+p = PrometheusQueryClient(url="http://192.168.39.20:31565")
+init_window(p; query="irate(container_cpu_usage_seconds_total[1m]) != 0", series_limit=25)
--- frontend_gui.jl
+++ frontend_gui.jl
@@ -1,3 +1,4 @@
+using Dates
 using GLMakie
 using UnicodePlots
 
@@ -10,9 +11,26 @@ const FONT_COURIER = Makie.to_font("Courier")
 const MAX_LEGEND_ENTRY_CHAR_LIMIT = 40
 const MAX_LEGEND_CHAR_LIMIT = 140
 
+const UPDATE_RESOLUTION_OPTIONS = [
+    "1s",
+    "2s",
+    "5s",
+    "10s",
+    "30s",
+    "1m",
+    "2m",
+    "5m",
+    "10m",
+    "20m",
+    "30m",
+    "1h"
+]
+
 const RED::RGBAf = RGBAf(1.0, 0.0, 0.0, 1.0)
 const WHITE::RGBAf = RGBAf(1.0, 1.0, 1.0, 1.0)
+const BLUE::RGBAf = RGBAf(0.0, 0.0, 1.0, 1.0)
 const VERY_DARK::RGBAf = RGBAf(0.1, 0.1, 0.1, 1.0)
+const BLACK::RGBAf = RGBAf(0.0, 0.0, 0.0, 1.0)
 
 const NORMAL_CURSOR = GLMakie.GLFW.CreateStandardCursor(GLMakie.GLFW.ARROW_CURSOR)
 const HAND_CURSOR = GLMakie.GLFW.CreateStandardCursor(GLMakie.GLFW.HAND_CURSOR)
@@ -63,8 +81,20 @@ function format_label_table(labels::Vector{String})::Vector{String}
     return formatted_labels
 end
 
-function render_data(fig, layout, ax, df, status_label; page=1, is3d=false, series_limit=100)
-    try empty!(ax); catch _; end;
+function render_data(fig, layout, ax, df, status_label; query=nothing, startdate=nothing, enddate=nothing, page=1, is3d=false, series_limit=100)
+    try 
+        # 3D plots don't have an empty! method defined, so let's clear the 
+        # whole axis and start anew
+        empty!(ax)
+
+        if is3d
+            delete!(ax)
+            ax = is3d ? 
+                Axis3(layout[1, 1]) : 
+                Axis(layout[1, 1])
+        end
+    catch;
+    end
 
     dfgroups = groupby(df, allbut(df, [:ts, :value]))
     series = []
@@ -99,6 +129,12 @@ function render_data(fig, layout, ax, df, status_label; page=1, is3d=false, seri
         end
     end
 
+    inspector = DataInspector(fig; range=0, enabled=false)
+
+    ax.title[] = join(("Query: " * query, "Start: " * startdate * ", End: " * enddate), "\n")
+    ax.titlesize[] = 24
+    ax.titlefont[] = FONT_COURIER
+
     ax.xlabel[] = "Time (s)"
     if is3d
         ax.ylabel[] = "Series Index"
@@ -129,34 +165,48 @@ function render_data(fig, layout, ax, df, status_label; page=1, is3d=false, seri
         series_indexes[s] = idx
     end
 
-
-    # Pagination for the Legend
-    prev_button = Button(layout[2, 2]; label="Previous", buttoncolor=:blue)
-    next_button = Button(layout[2, 2]; label="Next", buttoncolor=:blue)
+    block_series = Dict()
+    for (b, s) in zip(toggles, series)
+        block_series[b] = s
+    end
 
     layout[1, 2] = grid!(hcat(toggles, formatted_labels))
-    layout[2, 2] = grid!(hcat(prev_button, next_button))
-
     colsize!(layout, 1, Aspect(1, 1))
 
     glscreen = display(fig).glscreen
 
     for b in toggles
+        has_toggled_legend = false
+
         on(b.clicks) do e
             # Scan all of the active toggles and hide series that are inactive.
             # We show all series if all toggles are set to inactive.
             if all(x -> x.clicks.val % 2 == 0, toggles)
+                has_toggled_legend = false
+
                 for (idx, t) in enumerate(toggles)
                     series_visible[idx][] = true
                     t.buttoncolor[] = RGBAf(t.buttoncolor.val.r, t.buttoncolor.val.g, t.buttoncolor.val.b, 1.0)
                 end
-                return Consume(false)
+
+                reset_limits!(ax)
+                return Consume(true)
             end
+
+            # Keep track if we've made any changes, so that we can reset the
+            # limits of the graph if there are new graphs
+            has_update = false
 
             for (idx, t) in enumerate(toggles)
                 # The number of clicks will be odd if the toggle is active
                 is_active = t.clicks[] % 2 == 1
+                has_toggled_legend = has_toggled_legend || is_active
+
+                previous_active = series_visible[idx].val
                 series_visible[idx][] = is_active
+
+                has_update = has_update || previous_active != is_active
+
                 t.buttoncolor[] = RGBAf(
                     t.buttoncolor.val.r,
                     t.buttoncolor.val.g,
@@ -164,24 +214,50 @@ function render_data(fig, layout, ax, df, status_label; page=1, is3d=false, seri
                     is_active ? 1.0 : 0.25)
             end
 
-            return Consume(false)
+            if has_update
+                reset_limits!(ax)
+            end
+
+            return Consume(true)
         end
 
         active = nothing
         requires_reset = false
+
         on(events(fig).mouseposition) do m
             p, _ = pick(fig)
+            block_mouseover = try mouseover(b.blockscene, b.blockscene.children[begin].plots[begin]); catch _; false; end;
+
+            if block_mouseover
+                # Emulate a picked plot, the rest of the code should handle it gracefully
+                p = block_series[b]
+                requires_reset = false
+            end
 
             if active != p && !isnothing(active)
+                for s in series
+                    s.alpha[] = 1.0
+                end
+                
                 active.linewidth[] = 1.5
+                active.overdraw[] = false
                 active = nothing
             end
 
             if isnothing(p)
                 GLMakie.GLFW.SetCursor(glscreen, NORMAL_CURSOR)
                 if requires_reset
-                    for t in toggles 
-                        t.buttoncolor[] = RGBAf(t.buttoncolor.val.r, t.buttoncolor.val.g, t.buttoncolor.val.b, 1.0)
+                    all_off = all(x -> x.clicks.val % 2 == 0, toggles)
+                    for t in toggles
+                        t.buttoncolor[] = RGBAf(
+                            t.buttoncolor.val.r, 
+                            t.buttoncolor.val.g, 
+                            t.buttoncolor.val.b, 
+                            all_off || t.clicks.val % 2 == 1 ? 1.0 : 0.25
+                        )
+                    end
+                    for s in series
+                        s.alpha[] = 1.0
                     end
                 end
 
@@ -190,10 +266,16 @@ function render_data(fig, layout, ax, df, status_label; page=1, is3d=false, seri
             end
 
             if p in series
-                p.linewidth[] = 5.0
-                active = p
-                GLMakie.GLFW.SetCursor(glscreen, HAND_CURSOR)
                 requires_reset = true
+
+                active = p
+
+                p.linewidth[] = 5.0
+                p.overdraw[] = true
+
+                Makie.enable!(inspector)
+
+                GLMakie.GLFW.SetCursor(glscreen, HAND_CURSOR)
                 # Darken the rest of the legends to show which value we have selected
                 seriesidx = series_indexes[p]
 
@@ -202,16 +284,40 @@ function render_data(fig, layout, ax, df, status_label; page=1, is3d=false, seri
                         t.buttoncolor.val.r,
                         t.buttoncolor.val.g,
                         t.buttoncolor.val.b,
-                        idx == seriesidx ? 1.0 : 0.25
+                        idx == seriesidx || t.clicks[] % 2 == 1 ? 1.0 : 0.25
                     )
+                end
+
+                for s in series
+                    if s != p
+                        s.alpha[] = 0.5
+                    end
                 end
             end
 
             return Consume(false)
         end
+
+        on(events(fig).mousebutton) do mc
+            # Save the variable to avoid potentially losing the reference
+            current_active = active
+            if !isnothing(current_active)
+                if mc.button == Mouse.left && mc.action == Mouse.release
+                    # Toggle the series visibility by triggering the legend event handler
+                    update_block = toggles[series_indexes[current_active]]
+                    update_block.clicks[] += 1
+
+                    return Consume(true)
+                end
+            end
+        end
     end
 
-    return vcat(toggles, formatted_labels, prev_button, next_button)
+    return vcat(
+        toggles, 
+        formatted_labels, 
+        inspector
+    )
 end
 
 function validate_df(df, status_label; update_text::Bool=true)::Bool
@@ -230,7 +336,15 @@ function validate_df(df, status_label; update_text::Bool=true)::Bool
     return true
 end
 
-function init_window(p::PrometheusQueryClient; is3d=false, series_limit=100)
+function init_window(
+    p::PrometheusQueryClient;
+    query::Union{Nothing, String}=nothing,
+    is3d::Bool=false,
+    startdate::Union{Nothing, Union{DateTime, String}}=nothing,
+    enddate::Union{Nothing, Union{DateTime, String}}=nothing,
+    update_resolution::Union{Nothing, String}=nothing,
+    series_limit::Int=100
+)
     initial_resolution = (3840, 2160)
 
     GLMakie.activate!(; framerate=60.0)
@@ -239,7 +353,7 @@ function init_window(p::PrometheusQueryClient; is3d=false, series_limit=100)
     fig = Figure(resolution=initial_resolution)
 
     plot_layout = GridLayout(1, 2)
-    user_layout = GridLayout(3, 1)
+    user_layout = GridLayout(4, 1)
 
     fig[1, 1] = plot_layout
     fig[2, 1] = user_layout
@@ -264,16 +378,76 @@ function init_window(p::PrometheusQueryClient; is3d=false, series_limit=100)
         labelcolor=:white,
     )
 
+    if isnothing(update_resolution)
+        update_resolution = "5s"
+    end
+
+    update_interval_dropdown = Menu(
+        fig;
+        options=UPDATE_RESOLUTION_OPTIONS,
+        default=update_resolution,
+        textcolor=WHITE,
+        cell_color_active=BLUE,
+        selection_cell_color_inactive=BLUE,
+        cell_color_inactive_even=VERY_DARK,
+        cell_color_inactive_odd=VERY_DARK,
+        cell_color_hover=BLUE,
+        dropdown_arrow_color=WHITE,
+
+        width=60,
+        height=Auto(),
+    )
+
+    if isnothing(startdate)
+        startdate = string(now(UTC) - Dates.Hour(2)) * "Z"
+    end
+    startdate_tb = Textbox(
+        fig;
+        placeholder="Start Date",
+        stored_string=startdate,
+        focused=false,
+        fontsize=16,
+        font=FONT_COURIER,
+        width=250,
+        tellwidth=false,
+        halign=:left
+    )
+
+    if isnothing(enddate)
+        enddate = string(now(UTC)) * "Z"
+    end
+    enddate_tb = tb = Textbox(
+        fig;
+        placeholder="End Date",
+        stored_string=enddate,
+        focused=false,
+        fontsize=16,
+        font=FONT_COURIER,
+        width=250,
+        tellwidth=false,
+        halign=:left
+    )
+
     user_layout[1, 1] = grid!(
         hcat(
             is3d_button,
             live_mode_button,
+            Label(fig; text="Step:"),
+            update_interval_dropdown
+        )
+    )
+
+    user_layout[2, 1] = grid!(
+        vcat(
+            hcat(Label(fig; text="Start:"), startdate_tb),
+            hcat(Label(fig; text="End:"), enddate_tb)
         )
     )
 
     tb = Textbox(
-        user_layout[2, 1]; 
-        placeholder="PromQL Query (e.g. `irate(container_cpu_usage_seconds_total{container_name=\"prometheus\"}[5m])`)",
+        user_layout[3, 1]; 
+        placeholder="PromQL Query",
+        stored_string=query,
         focused=true,
         fontsize=24,
         font=FONT_COURIER,
@@ -281,7 +455,7 @@ function init_window(p::PrometheusQueryClient; is3d=false, series_limit=100)
         tellwidth=false,
     )
     status_label = Label(
-        user_layout[3, 1];
+        user_layout[4, 1];
         text=" ",
         font=FONT_COURIER,
         fontsize=16,
@@ -294,6 +468,10 @@ function init_window(p::PrometheusQueryClient; is3d=false, series_limit=100)
     is3d = false
 
     on(is3d_button.clicks) do clicks
+        # Sleep to allow the button to revert back to its original appearance
+        sleep(0.1)
+
+        # Wipe out the previous Axis(3)
         delete!(ax)
 
         is3d = clicks % 2 != 0
@@ -309,24 +487,50 @@ function init_window(p::PrometheusQueryClient; is3d=false, series_limit=100)
         end
 
         if validate_df(df, status_label; update_text=false)
+            # TODO: we can put garbage collection into a function since it is duplicated
+            # below in the keyboard event handler
             if !isnothing(gc)
                 for g in gc
-                    delete!(g)
+                    try delete!(g); catch _; end;
                 end
             end
-            gc = render_data(fig, plot_layout, ax, df, status_label, is3d=is3d, series_limit=series_limit)
+
+            startdate = startdate_tb.stored_string.val
+            enddate = enddate_tb.stored_string.val
+
+            gc = render_data(
+                fig, 
+                plot_layout, 
+                ax, 
+                df, 
+                status_label,
+                query=tb.stored_string[],
+                startdate=startdate,
+                enddate=enddate,
+                is3d=is3d, 
+                series_limit=series_limit
+            )
         end
 
         return Consume(false)
     end
 
-    on(events(fig).keyboardbutton) do e
-        if e.action !== Keyboard.release
+    update_query = on(events(fig).keyboardbutton, update=!isnothing(query)) do e
+        if !isnothing(e) && e.action !== Keyboard.release
             return Consume(false)
         end
 
+        startdate = startdate_tb.stored_string.val
+        enddate = enddate_tb.stored_string.val
+
         try 
-            df = promql(p, tb.stored_string[])
+            df = promql(
+                p, 
+                tb.stored_string[];
+                startdate=startdate,
+                enddate=enddate,
+                step=update_interval_dropdown.selection[]
+            )
         catch err
             status_label.color[] = RED
             status_label.text[] = "$err"
@@ -336,12 +540,28 @@ function init_window(p::PrometheusQueryClient; is3d=false, series_limit=100)
         if validate_df(df, status_label; update_text=true)
             if !isnothing(gc)
                 for g in gc
-                    delete!(g)
+                    try delete!(g); catch; end;
                 end
             end
-            gc = render_data(fig, plot_layout, ax, df, status_label, is3d=is3d, series_limit=series_limit)
+            gc = render_data(
+                fig,
+                plot_layout,
+                ax,
+                df,
+                status_label,
+                query=tb.stored_string[],
+                startdate=startdate,
+                enddate=enddate,
+                is3d=is3d,
+                series_limit=series_limit
+            )
         end
 
+        return Consume(false)
+    end
+
+    on(update_interval_dropdown.selection) do selection
+        update_query.f(nothing)
         return Consume(false)
     end
 
